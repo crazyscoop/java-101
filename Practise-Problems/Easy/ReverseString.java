89c27062b8aeb22417dee89152b60c50c47a7aac
himanshusinghchauhan <himanshusingh.chauhan2015@vit.ac.in>
2021-02-06 20:58:36 +0530

    Reverse String using  Recursion

---
+++ Practise-Problems/Easy/ReverseString.java
@@ -0,0 +1,22 @@
+public class ReverseString {
+
+  public static void main(String... args) {
+
+    System.out.println(reverse("abcde".toCharArray(), 0));
+  }
+
+  private static String reverse(char[] name, int index) {
+
+    if (index < name.length/2) {
+      swapAtIndex(name, index);
+      return reverse(name, ++index);
+    }
+    return new String(name);
+  }
+
+  private static void swapAtIndex(char[] array, int index) {
+    char temp = array[index];
+    array[index] = array[array.length - index - 1];
+    array[array.length - index - 1] = temp;
+  }
+}
